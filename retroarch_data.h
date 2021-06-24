82c3df7f7aef302f436602823b11569a020aaeec
John Parton <john.parton.iv@gmail.com>
2021-06-24 17:27:00 -0500

    Enable alsa on rs90

--- retroarch_data.h
+++ retroarch_data.h
@@ -995,7 +995,7 @@ static input_device_driver_t *joypad_drivers[] = {
 };
 
 #ifdef HAVE_HID
-static bool null_hid_joypad_query(void *data, unsigned pad) { 
+static bool null_hid_joypad_query(void *data, unsigned pad) {
    return pad < MAX_USERS; }
 static const char *null_hid_joypad_name(
       void *data, unsigned pad) { return NULL; }
@@ -1190,7 +1190,7 @@ static midi_driver_t midi_null = {
 };
 
 static midi_driver_t *midi_drivers[] = {
-#if defined(HAVE_ALSA) && !defined(HAVE_HAKCHI) && !defined(HAVE_SEGAM)
+#if defined(HAVE_ALSA) && !defined(HAVE_HAKCHI) && !defined(HAVE_SEGAM) && !defined(RS90)
    &midi_alsa,
 #endif
 #ifdef HAVE_WINMM
@@ -1665,7 +1665,7 @@ typedef struct discord_state discord_state_t;
 #endif
 
 struct runloop
-{ 
+{
    retro_usec_t frame_time_last;        /* int64_t alignment */
 
    msg_queue_t msg_queue;                        /* ptr alignment */
@@ -1733,7 +1733,7 @@ struct rarch_state
    menu_input_t menu_input_state;               /* retro_time_t alignment */
 #endif
 
-   
+
 
 #if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_SLANG) || defined(HAVE_HLSL)
    rarch_timer_t shader_delay_timer;            /* int64_t alignment */
@@ -1911,7 +1911,7 @@ struct rarch_state
    struct retro_subsystem_rom_info
       subsystem_data_roms[SUBSYSTEM_MAX_SUBSYSTEMS]
       [SUBSYSTEM_MAX_SUBSYSTEM_ROMS];                    /* ptr alignment */
-   
+
    gfx_ctx_driver_t current_video_context;               /* ptr alignment */
    content_state_t            content_st;                /* ptr alignment */
    midi_event_t midi_drv_input_event;                    /* ptr alignment */
@@ -1989,7 +1989,7 @@ struct rarch_state
    size_t runahead_save_state_size;
 #endif
 
-   jmp_buf error_sjlj_context;              /* 4-byte alignment, 
+   jmp_buf error_sjlj_context;              /* 4-byte alignment,
                                                put it right before long */
 
    turbo_buttons_t input_driver_turbo_btns; /* int32_t alignment */
@@ -2017,7 +2017,7 @@ struct rarch_state
    int reannounce;
 #endif
 
-   input_device_info_t input_device_info[MAX_INPUT_DEVICES]; 
+   input_device_info_t input_device_info[MAX_INPUT_DEVICES];
                                           /* unsigned alignment */
 #ifdef HAVE_MENU
    menu_dialog_t dialog_st;               /* unsigned alignment */
@@ -2093,7 +2093,7 @@ struct rarch_state
 #endif
 
 #ifdef HAVE_MENU
-   menu_input_pointer_hw_state_t menu_input_pointer_hw_state;  
+   menu_input_pointer_hw_state_t menu_input_pointer_hw_state;
                                                 /* int16_t alignment */
 #endif
 
